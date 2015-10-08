2e043c7dc3e580e381a8fa0c094112395ada0ff1
Raul B. Barbosa <raul.barbosa@unirio.br>
2015-10-08 17:35:06 -0300

    Trocando de materialized para view de novo

--- SIEProjetosPesquisa.py
+++ SIEProjetosPesquisa.py
@@ -262,7 +262,7 @@ class SIEProjetosPesquisa(SIEProjetos):
 
         #fields = ['NOME','ID_PESSOA','MATRICULA','DESCRICAO_VINCULO']
         try:
-            res = self.api.get("MT_PROJETOS_PESSOAS", params, cached=0)
+            res = self.api.get("V_PROJETOS_PESSOAS", params, cached=0)
             return res.content if res is not None else []
         except ValueError:
             return []
@@ -309,7 +309,7 @@ class SIEProjetosPesquisa(SIEProjetos):
             })
 
         try:
-            res = self.api.get("MT_PROJETOS_PESSOAS", params, cached=0)
+            res = self.api.get("V_PROJETOS_PESSOAS", params, cached=0)
             return res.content[0] if res is not None else {}
         except ValueError:
             return {}
