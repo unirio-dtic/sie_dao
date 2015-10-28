25eb7e628ad7adbec4bcbc7ee2ef8eda16478d65
Hydren <5carlosfelipe5@gmail.com>
2015-10-28 14:09:31 -0200

    Melhorias no teste de SIEDocumento.py

--- tests/Documento.py
+++ tests/Documento.py
@@ -4,10 +4,14 @@ __author__ = 'carlosfaruolo'
 
 
 class TestDocumento(SIETestCase):
+    DUMMY_INVALID_ID = 857893245934
 
     def __init__(self, *args, **kwargs):
         super(TestDocumento, self).__init__(*args, **kwargs)
+
         from sie.SIEDocumento import SIEDocumentoDAO
+
+        self.documento_valido = self.api.get(SIEDocumentoDAO.path).first()
 
     def setUp(self):
         from sie.SIEDocumento import SIEDocumentoDAO
@@ -17,7 +21,27 @@ class TestDocumento(SIETestCase):
         documento = self.dao.obter_documento(2654)
         self.assertIsInstance(documento, dict)
 
-    def test_falhar_em_obter_documento(self):
+    def test_obter_documento_id_errado(self):
         with self.assertRaises(Exception):
-            documento = self.dao.obter_documento(3463445687964795689578057846798)
+            documento = self.dao.obter_documento(self.DUMMY_INVALID_ID)
 
+    # TODO fazer esses abaixo:
+
+    def test_obter_tramitacao_atual(self):
+        self.fail()
+
+    # criar_documento()
+
+    def test_criar_documento(self):
+        self.fail()
+
+    def test_criar_documento_tipo_errado(self):
+        self.fail()
+
+    def test_obter_fluxo_tramitacao_atual(self):
+        self.fail()
+
+    def test_obter_proximos_fluxos_tramitacoes_atual(self):
+        self.fail()
+
+    # more TODO ...
