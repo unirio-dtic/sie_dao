0d060cc0ef67120398b69bb17e71f528c7ec714d
Hydren <5carlosfelipe5@gmail.com>
2015-11-04 16:31:12 -0200

    Atualizado arquivo de testes de SIEDocumento

--- tests/Documento.py
+++ tests/Documento.py
@@ -10,8 +10,10 @@ class TestDocumento(SIETestCase):
         super(TestDocumento, self).__init__(*args, **kwargs)
 
         from sie.SIEDocumento import SIEDocumentoDAO
+        from sie.SIEFuncionarios import SIEFuncionarioID
 
         self.documento_valido = self.api.get(SIEDocumentoDAO.path).first()
+        self.funcionario_dummy = self.api.get(SIEFuncionarioID.path).first()
 
     def setUp(self):
         from sie.SIEDocumento import SIEDocumentoDAO
@@ -25,18 +27,23 @@ class TestDocumento(SIETestCase):
         with self.assertRaises(Exception):
             documento = self.dao.obter_documento(self.DUMMY_INVALID_ID)
 
+    def test_criar_documento_projeto_pesquisa(self):
+        from sie.SIEProjetosPesquisa import SIEProjetosPesquisa
+        dao_projetos = SIEProjetosPesquisa()
+        documento = self.dao.criar_documento(dao_projetos.documento_inicial_padrao(self.funcionario_dummy))
+        self.assertIsInstance(documento, dict)
+        self.dao.remover_documento(documento)  # clean poopie
+
     # TODO fazer esses abaixo:
 
+    def test_criar_documento_params_vazios(self):
+        with self.assertRaises(KeyError):
+            from sie.SIEProjetos import SIEProjetos
+            dao_projetos = SIEProjetos()
+            documento = self.dao.criar_documento(dict())
+
     def test_obter_tramitacao_atual(self):
-        self.fail()
-
-    # criar_documento()
-
-    def test_criar_documento(self):
-        self.fail()
-
-    def test_criar_documento_tipo_errado(self):
-        self.fail()
+        self.assertIsInstance(self.dao.obter_tramitacao_atual(self.documento_valido), dict)
 
     def test_obter_fluxo_tramitacao_atual(self):
         self.fail()
@@ -44,4 +51,4 @@ class TestDocumento(SIETestCase):
     def test_obter_proximos_fluxos_tramitacoes_atual(self):
         self.fail()
 
-    # more TODO ...
+        # more TODO ...
